dcd3dffa18a43dd02fe82a1385aca49d06741a28
Pavel Karpenko <pwkpwk@hotmail.com>
2025-07-26 00:21:46 -0700

    Pick from an array

--- Rendezvous/IRendezvousPicker.cs
+++ Rendezvous/IRendezvousPicker.cs
@@ -2,8 +2,27 @@
 
 public interface IRendezvousPicker<in TKey>
 {
-    ReadOnlySpan<char> Pick(ReadOnlySpan<char> commaSeparatedList, TKey key);
+    /// <summary>
+    /// Pick one of the comma-separated tokens from the span by Rendezvous hashing with the specified key.
+    /// </summary>
+    /// <param name="tokens">Comma-separated list of tokens</param>
+    /// <param name="key">Key used to consistently pick a random token from the span</param>
+    /// <returns>One of the tokens from the span array picked randomly but consistently for the key</returns>
+    /// <remarks>Order of tokens in the span does not matter; the algorithm always picks the same token
+    /// for the same value of the key.</remarks>
+    ReadOnlySpan<char> Pick(ReadOnlySpan<char> tokens, TKey key);
 
-    sealed ReadOnlySpan<char> Pick(string commaSeparatedList, TKey key) =>
-        Pick(commaSeparatedList.AsSpan(), key);
+    /// Same as <see cref="Pick(System.ReadOnlySpan{char},TKey)"/> but takes tokens as a string
+    sealed ReadOnlySpan<char> Pick(string tokens, TKey key) => Pick(tokens.AsSpan(), key);
+
+    /// <summary>
+    /// Pick one of the names from an array by Rendezvous hashing with the specified key.
+    /// </summary>
+    /// <param name="names">Non-empty array of names</param>
+    /// <param name="key">Key used to consistently pick a random name from the array</param>
+    /// <returns>One of the strings from the names array picked randomly but consistently for the key</returns>
+    /// <exception cref="ArgumentException">Thrown if the array of names is empty</exception>
+    /// <remarks>Order of strings in the array does not matter; the algorithm always picks the same string
+    /// for the same value of the key.</remarks>
+    string Pick(string[] names, TKey key);
 }
---
+++ Rendezvous/PolynomialRollingHash32Bit.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Rendezvous;
+
+public class PolynomialRollingHash32Bit : IPickerHash
+{
+    private const uint P = 31;
+    private const uint M = 1000000009;
+
+    uint IPickerHash.Calculate(uint seed, ReadOnlySpan<byte> data)
+    {
+        ref byte bp = ref MemoryMarshal.GetReference(data);
+            
+        ulong hash = 0;
+        ulong ppow = 1;
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            hash = (hash + bp * ppow) % M;
+            ppow = ppow * P % M;
+            bp = ref Unsafe.Add(ref bp, 1);
+        }
+            
+        hash  = (hash + seed * ppow) % M;
+
+        return (uint)(hash ^ (hash >> 32));
+    }
+}
